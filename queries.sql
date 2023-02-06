24864f3cdcf4aeb72fe33cfed5f56ea895d0ee60
melodybuezo <90790916+melodybuezo@users.noreply.github.com>
2023-02-05 21:45:59 -0600

    Add files via upload

---
+++ queries.sql
@@ -0,0 +1,5 @@
+CREATE TABLE MelodyBooks (id INTEGER, book TEXT, starbies TEXT);
+INSERT INTO MelodyBooks VALUES(1,"A little life", "Chai Tea");
+INSERT INTO MelodyBooks VALUES(2,"Frankenstein", "Matcha Latte");
+INSERT INTO MelodyBooks VALUES(3,"The Myth of Sysyiphus", "White Hot Chocolate");
+SELECT * FROM MelodyBooks
